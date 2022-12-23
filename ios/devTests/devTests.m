ede80b3ed025a51fe96a5a1da6080326f61d1e4f
YashKorat17 <yashkorat17@gmail.com>
2022-12-23 18:28:17 +0530

    All Design Change

--- ios/devTests/devTests.m
+++ ios/devTests/devTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface devTests : XCTestCase
+@interface DevTests : XCTestCase
 
 @end
 
-@implementation devTests
+@implementation DevTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
